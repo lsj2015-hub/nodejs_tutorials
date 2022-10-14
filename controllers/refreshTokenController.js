156d0ce7e6bd99cef75e11c0dfaf7360980204a5
lsj2015-hub <lsj2015@hotmail.com>
2022-10-14 16:43:51 +0900

    until logout including jwt

---
+++ controllers/refreshTokenController.js
@@ -0,0 +1,35 @@
+// client가 보내온 refreshToken이 usersDB의 user안에 있는 refreshToken과 일치하는지 확인하는 controller
+const usersDB = {
+  users: require('../model/users.json'),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const handleRefreshToken = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(401);
+  console.log(cookies.jwt);
+  const refreshToken = cookies.jwt;
+
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken
+  );
+  if (!foundUser) return res.sendStatus(401); // Forbidden
+  // evaluate jwt
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username)
+      return res.sendStatus(403);
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '30s' }
+    );
+    res.json({ accessToken });
+  });
+};
+
+module.exports = { handleRefreshToken };
